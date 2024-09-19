58d870cfacd85535c6ceaeb53cfa017918e7c8f6
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-19 16:30:50 +0100

    Create MainOeuvre.java

---
+++ src/entity/MainOeuvre.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class MainOeuvre extends Composant {
+    private double tauxHoraire;
+    private double heuresTravail;
+    private double productiviteOuvrier;
+
+    public MainOeuvre(String nom, String typeComposant, double tauxTVA, double tauxHoraire, double heuresTravail,
+            double productiviteOuvrier) {
+        super(nom, typeComposant, tauxTVA);
+        this.tauxHoraire = tauxHoraire;
+        this.heuresTravail = heuresTravail;
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    // Getters and Setters
+    public double getTauxHoraire() {
+        return tauxHoraire;
+    }
+
+    public void setTauxHoraire(double tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    public double getHeuresTravail() {
+        return heuresTravail;
+    }
+
+    public void setHeuresTravail(double heuresTravail) {
+        this.heuresTravail = heuresTravail;
+    }
+
+    public double getProductiviteOuvrier() {
+        return productiviteOuvrier;
+    }
+
+    public void setProductiviteOuvrier(double productiviteOuvrier) {
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+}
