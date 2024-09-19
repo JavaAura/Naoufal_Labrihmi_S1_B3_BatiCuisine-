1852db902d7bf654ef92c47e6e1a1a844bec1f65
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-19 16:30:44 +0100

    Create Devis.java

---
+++ src/entity/Devis.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.util.Date;
+
+public class Devis {
+    private double montantEstime;
+    private Date dateEmission;
+    private Date dateValidite;
+    private boolean accepte;
+
+    public Devis(double montantEstime, Date dateEmission, Date dateValidite, boolean accepte) {
+        this.montantEstime = montantEstime;
+        this.dateEmission = dateEmission;
+        this.dateValidite = dateValidite;
+        this.accepte = accepte;
+    }
+
+    // Getters and Setters
+    public double getMontantEstime() {
+        return montantEstime;
+    }
+
+    public void setMontantEstime(double montantEstime) {
+        this.montantEstime = montantEstime;
+    }
+
+    public Date getDateEmission() {
+        return dateEmission;
+    }
+
+    public void setDateEmission(Date dateEmission) {
+        this.dateEmission = dateEmission;
+    }
+
+    public Date getDateValidite() {
+        return dateValidite;
+    }
+
+    public void setDateValidite(Date dateValidite) {
+        this.dateValidite = dateValidite;
+    }
+
+    public boolean isAccepte() {
+        return accepte;
+    }
+
+    public void setAccepte(boolean accepte) {
+        this.accepte = accepte;
+    }
+}
