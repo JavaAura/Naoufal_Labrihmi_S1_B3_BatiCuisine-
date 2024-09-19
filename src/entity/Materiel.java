99601a0d9eac0b1c2fd241d8fed87ee266c814d2
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-19 16:30:52 +0100

    Create Materiel.java

---
+++ src/entity/Materiel.java
@@ -0,0 +1,50 @@
+package entity;
+
+public class Materiel extends Composant {
+    private double coutUnitaire;
+    private double quantite;
+    private double coutTransport;
+    private double coefficientQualite;
+
+    public Materiel(String nom, String typeComposant, double tauxTVA, double coutUnitaire, double quantite,
+            double coutTransport, double coefficientQualite) {
+        super(nom, typeComposant, tauxTVA);
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
+        this.coutTransport = coutTransport;
+        this.coefficientQualite = coefficientQualite;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+
+    public void setCoutUnitaire(double coutUnitaire) {
+        this.coutUnitaire = coutUnitaire;
+    }
+
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public double getCoutTransport() {
+        return coutTransport;
+    }
+
+    public void setCoutTransport(double coutTransport) {
+        this.coutTransport = coutTransport;
+    }
+
+    public double getCoefficientQualite() {
+        return coefficientQualite;
+    }
+
+    public void setCoefficientQualite(double coefficientQualite) {
+        this.coefficientQualite = coefficientQualite;
+    }
+
+}
