0239642a8f032b67bd752def17f521859defae8f
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-19 16:30:54 +0100

    Create Projet.java

---
+++ src/entity/Projet.java
@@ -0,0 +1,82 @@
+package entity;
+
+import entity.enums.EtatProjet;
+import java.util.List;
+
+public class Projet {
+    private String nomProjet;
+    private double margeBeneficiaire;
+    private double coutTotal;
+    private EtatProjet etatProjet;
+    private Client client;
+    private List<Composant> composants;
+    private Devis devis;
+
+    public Projet(String nomProjet, double margeBeneficiaire, double coutTotal, EtatProjet etatProjet, Client client,
+            List<Composant> composants, Devis devis) {
+        this.nomProjet = nomProjet;
+        this.margeBeneficiaire = margeBeneficiaire;
+        this.coutTotal = coutTotal;
+        this.etatProjet = etatProjet;
+        this.client = client;
+        this.composants = composants;
+        this.devis = devis;
+    }
+
+    // Getters and Setters
+    public String getNomProjet() {
+        return nomProjet;
+    }
+
+    public void setNomProjet(String nomProjet) {
+        this.nomProjet = nomProjet;
+    }
+
+    public double getMargeBeneficiaire() {
+        return margeBeneficiaire;
+    }
+
+    public void setMargeBeneficiaire(double margeBeneficiaire) {
+        this.margeBeneficiaire = margeBeneficiaire;
+    }
+
+    public double getCoutTotal() {
+        return coutTotal;
+    }
+
+    public void setCoutTotal(double coutTotal) {
+        this.coutTotal = coutTotal;
+    }
+
+    public EtatProjet getEtatProjet() {
+        return etatProjet;
+    }
+
+    public void setEtatProjet(EtatProjet etatProjet) {
+        this.etatProjet = etatProjet;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Composant> getComposants() {
+        return composants;
+    }
+
+    public void setComposants(List<Composant> composants) {
+        this.composants = composants;
+    }
+
+    public Devis getDevis() {
+        return devis;
+    }
+
+    public void setDevis(Devis devis) {
+        this.devis = devis;
+    }
+}
