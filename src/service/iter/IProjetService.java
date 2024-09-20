0d8a34d84877b444a6043f9ce750770d51561d8a
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-20 17:08:43 +0100

    Create IProjetService.java

---
+++ src/service/iter/IProjetService.java
@@ -0,0 +1,14 @@
+package service.iter;
+
+import java.util.List;
+
+import entity.Projet;
+
+public interface IProjetService {
+    void addProject(Projet projet);
+
+    List<Projet> getAllProjets();
+
+    Projet getProjectById(Long id);
+
+}
