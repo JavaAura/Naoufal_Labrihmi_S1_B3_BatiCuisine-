50bdc9d3a1121bd1c55b76db26cd79a7b3e3274f
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-20 17:09:15 +0100

    Create ProjetServiceImpl.java

---
+++ src/service/impl/ProjetServiceImpl.java
@@ -0,0 +1,30 @@
+package service.impl;
+
+import java.util.List;
+
+import entity.Projet;
+import repository.inter.IProjetRepository;
+import service.iter.IProjetService;
+
+public class ProjetServiceImpl implements IProjetService {
+    private final IProjetRepository projetRepository;
+
+    public ProjetServiceImpl(IProjetRepository projetRepository) {
+        this.projetRepository = projetRepository;
+    }
+
+    @Override
+    public void addProject(Projet projet) {
+        projetRepository.addProject(projet);
+    }
+
+    @Override
+    public List<Projet> getAllProjets() {
+        return projetRepository.getAllProjets();
+    }
+
+    @Override
+    public Projet getProjectById(Long id) {
+        return projetRepository.getProjectById(id);
+    }
+}
