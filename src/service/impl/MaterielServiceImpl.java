bc11586c476c33cb924602f081723bf989738d50
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-20 17:08:40 +0100

    Create MaterielServiceImpl.java

---
+++ src/service/impl/MaterielServiceImpl.java
@@ -0,0 +1,18 @@
+package service.impl;
+
+import entity.Materiel;
+import repository.inter.IComposantRepository;
+import service.iter.IComposantService;
+
+public class MaterielServiceImpl implements IComposantService<Materiel> {
+    private final IComposantRepository<Materiel> materielRepository;
+
+    public MaterielServiceImpl(IComposantRepository<Materiel> materielRepository) {
+        this.materielRepository = materielRepository;
+    }
+
+    @Override
+    public void addComposant(Materiel materiel) {
+        materielRepository.addComposant(materiel);
+    }
+}
