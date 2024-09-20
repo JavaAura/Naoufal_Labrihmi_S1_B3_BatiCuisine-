7fa9b075946dbd8a57266c1095ac84204b7bec00
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-20 17:08:17 +0100

    Create IDevisRepository.java

---
+++ src/repository/inter/IDevisRepository.java
@@ -0,0 +1,15 @@
+package repository.inter;
+
+import java.util.List;
+
+import entity.Devis;
+
+public interface IDevisRepository {
+    void addDevis(Devis devis);
+
+    // List<Devis> findAll();
+
+    // Devis findById(int id);
+
+    // void deleteById(int id);
+}
