2517e1c2110782d5b9932ea318d4bf31119267cb
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-23 03:53:07 +0100

    Update IDevisService.java

--- src/service/iter/IDevisService.java
+++ src/service/iter/IDevisService.java
@@ -4,4 +4,10 @@ import entity.Devis;
 
 public interface IDevisService {
     void addDevis(Devis devis);
+
+    Devis getDevisById(int id);
+
+    void updateAccDevis(Devis devis);
+
+    Devis findByProjetId(Long projetId);
 }
