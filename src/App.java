8981e5651359ca2edccf8ba988820de2a249ed7e
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-23 03:52:38 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,17 +1,25 @@
 import java.sql.Connection;
 
 import database.DatabaseConnection;
+import entity.MainOeuvre;
+import entity.Materiel;
 import controller.ClientController;
+import controller.ComposantController;
 import controller.DevisController;
 import controller.ProjetController;
 import repository.inter.IClientRepository;
+import repository.inter.IComposantRepository;
 import repository.inter.IProjetRepository;
 import repository.inter.IDevisRepository;
 import repository.memory.ClientRepositoryImpl;
 import repository.memory.ProjetRepositoryImpl;
 import repository.memory.DevisRepositoryImpl;
+import repository.memory.MainOeuvreRepositoryImpl;
+import repository.memory.MaterielRepositoryImpl;
 import service.impl.ClientServiceImpl;
 import service.impl.DevisServiceImpl;
+import service.impl.MainOeuvreServiceImpl;
+import service.impl.MaterielServiceImpl;
 import service.impl.ProjetServiceImpl;
 import view.ConsoleUI;
 
@@ -27,19 +35,26 @@ public class App {
             IClientRepository clientRepository = new ClientRepositoryImpl(connection);
             IProjetRepository projetRepository = new ProjetRepositoryImpl(connection);
             IDevisRepository devisRepository = new DevisRepositoryImpl(connection);
+            IComposantRepository<Materiel> materielRepository = new MaterielRepositoryImpl(connection);
+            IComposantRepository<MainOeuvre> mainOeuvreRepository = new MainOeuvreRepositoryImpl(connection);
 
             // Initialize the service implementations with repositories
             ClientServiceImpl clientService = new ClientServiceImpl(clientRepository);
             ProjetServiceImpl projetService = new ProjetServiceImpl(projetRepository);
             DevisServiceImpl devisService = new DevisServiceImpl(devisRepository);
+            MaterielServiceImpl materielService = new MaterielServiceImpl(materielRepository);
+            MainOeuvreServiceImpl mainOeuvreService = new MainOeuvreServiceImpl(mainOeuvreRepository);
 
             // Initialize the controllers with the injected services
             ClientController clientController = new ClientController(clientService);
             ProjetController projetController = new ProjetController(projetService, clientController);
             DevisController devisController = new DevisController(devisService, projetService);
+            ComposantController composantController = new ComposantController(materielService, mainOeuvreService,
+                    projetService);
 
             // Initialize the ConsoleUI with the controllers
-            ConsoleUI consoleUI = new ConsoleUI(projetController, devisController, clientController);
+            ConsoleUI consoleUI = new ConsoleUI(projetController, devisController, clientController,
+                    composantController);
             consoleUI.start();
 
         } catch (Exception e) {
