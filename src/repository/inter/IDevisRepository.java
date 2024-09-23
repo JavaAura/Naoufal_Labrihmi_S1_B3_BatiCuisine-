2fdc844770c9667723ac32bf7355fdce85ddde32
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-23 03:53:25 +0100

    Update IDevisRepository.java

--- src/repository/inter/IDevisRepository.java
+++ src/repository/inter/IDevisRepository.java
@@ -5,6 +5,13 @@ import entity.Devis;
 public interface IDevisRepository {
     void addDevis(Devis devis);
 
+    void updateAccDevis(Devis devis);
+
+    Devis getDevisById(int id);
+
+    Devis findByProjetId(Long projetId);
+
+
     // List<Devis> findAll();
 
     // Devis findById(int id);
