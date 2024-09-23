4358979018ff8bd9fda617cdc8004b4f98830b61
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-23 03:53:10 +0100

    Update IComposantService.java

--- src/service/iter/IComposantService.java
+++ src/service/iter/IComposantService.java
@@ -1,7 +1,12 @@
 package service.iter;
 
+import java.util.List;
+
 import entity.Composant;
 
 public interface IComposantService<T extends Composant> {
     void addComposant(T composant);
+
+    List<T> getAllComposantsByProject(Long projectId);
+
 }
